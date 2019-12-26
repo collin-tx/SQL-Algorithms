bb209c07a73d2a926b53570b539ed4915d3aa146
Collin Smith <cmsmith.tx@gmail.com>
2019-12-26 15:41:26 -0600

    adding solutions

---
+++ algs.sql
@@ -0,0 +1,8 @@
+-- Get average 'Price' from column 'OrderDetails' in DB
+SELECT AVG(Price) FROM OrderDetails;
+
+-- Get number of records from 'Orders' column in DB
+SELECT COUNT(OrderId) FROM Orders;
+
+-- Get sum of prices from 'OrderDetails' in DB
+SELECT SUM(Price) FROM OrderDetails;
